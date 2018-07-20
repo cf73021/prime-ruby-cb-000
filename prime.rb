22385632dc1b6d9c2e493bc93a64e011de588299
Chris Franklin <cf73021@gmail.com>
2018-07-20 17:27:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(number)
-  if n <= 1
+  if number <= 1
     return false
-  elsif n <= 3
+  elsif number <= 3
     return true
   else
     (2..number-1).to_a.all? do |i|
